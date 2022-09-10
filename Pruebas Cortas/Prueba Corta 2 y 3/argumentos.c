d6b303224498b596e067fb4b4979a68c7094f836
Jocxan S <jocxansandi7@estudiantec.cr>
2022-09-10 11:00:46 -0600

    Prueba corta 2 y3
    No lograda

---
+++ Pruebas Cortas/Prueba Corta 2 y 3/argumentos.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main(int argc, char *argv[]) {
+    if(argc >=2) {
+        printf("Length of string a = %i \n",strlen(argv[1]));
+        
+    }
+    return 0;
+}
---
+++ Pruebas Cortas/Prueba Corta 2 y 3/bitwise.c
@@ -0,0 +1,32 @@
+
+// C Program to demonstrate use of bitwise operators
+//https://www.programiz.com/c-programming/bitwise-operators
+#include <stdio.h>
+int main()
+{
+	// a = 5(00000101), b = 9(00001001)
+	unsigned char a = 5, b = 9;
+	char S[2]= "FA"; 
+	// The result is 00000001
+	printf("a = %d, b = %d\n", a, b);
+	printf("a&b = %d\n", a & b);
+
+	// The result is 00001101
+	printf("a|b = %d\n", a | b);
+
+	// The result is 00001100
+	printf("a^b = %d\n", a ^ b);
+
+	// The result is 11111010
+	printf("~a = %d\n", a = ~a);
+
+	// The result is 00010010
+	printf("b<<1 = %d\n", b << 1);
+
+	// The result is 00000100 
+	printf("b>>1 = %d\n", b >> 2);
+
+	return 0;
+
+}
+//11111010 10111010 00000110 00101100
